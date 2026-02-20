4d57ec73f678d08852e4a50ca179f0aa151e9fda
Umang <umangamrania@outlook.com>
2026-02-21 00:38:55 +0530

    refactor: parse_http & add: resp_404

--- http.asm
+++ http.asm
@@ -3,6 +3,7 @@ extern  strlen
 
 global  parse_http
 global  resp_400
+global  resp_404
 global  resp_200
 
 section .data
@@ -10,65 +11,63 @@ space_str:      db      ' ', 0
 
 ; response headers
 resp_header_400:        db      "HTTP/1.1 400 BAD REQUEST", 13, 10, 13, 10, 0
+resp_header_404:        db      "HTTP/1.1 404 NOT FOUND", 13, 10, 13, 10, 0
 resp_header_200:        db      "HTTP/1.1 200 OK", 13, 10, 13, 10, 0
 
 section .text
 
+; ***
+;  I am aware that this function is messy and can clearly be improved,
+; but it worked on first attempt :)
+; ***
+;
 ; --- PARSE_HTTP (char *s, char **method, char **path) ---
 
 parse_http:
-        ; Store arguments in stack for later use
-        push    rdi
-        push    rsi
+        push    rbp
+        mov     rbp, rsp
+
         push    rdx
+        push    rsi
+        push    rdi
 
-
-        ; find first space
+        ; rdi already contains *s
+        ; Finding first space that seperates method and path
         mov     rsi, space_str
         call    strstr
 
-
-        ; Return if space not found
         cmp     rax, 0
-        jl      .return
+        jl      .bad
 
+        ; get start of str and terminate at first space
+        pop     rdi
+        mov     byte [rdi + rax], 0                     ; terminate after method
 
-        ; replace space with 0
-        mov     rdi, qword [rsp + 8 * 2]                        ; load value of s from stack
-        mov     byte [rdi + rax], 0                             ; write 0
+        pop     rsi                                     ; pop **method from stack
+        mov     [rsi], rdi                              ; store start of buf at **method
 
-        ; store method address in **method ptr
-        mov     rsi, [rsp + 8]                                  ; load **method from stack
-        mov     [rsi], rdi
-
-
-        ; continue finding next space after first space
-        add     rdi, rax
+        add     rdi, rax                                ; set start address to after terminator
         inc     rdi
-        push    rdi                                             ; preserve string pointer
+        push    rdi                                     ; store new start of buf
+
+        ; find second space
         mov     rsi, space_str
         call    strstr
-        pop     rdi
 
-        ; Return if space not found
         cmp     rax, 0
-        jl      .return
+        jl      .bad
 
-        ; replace space with 0
-        mov     byte [rdi + rax], 0                             ; write 0
-
-        ; store method address in **path ptr
-        mov     rsi, [rsp]                                      ; load **path from stack
-        mov     [rsi], rdi
-
-        mov     rax, 0
-
-.return:
-        pop     rdx
-        pop     rsi
         pop     rdi
-        ret
+        mov     byte [rdi + rax], 0
 
+        pop     rdx
+        mov     [rdx], rdi
+        jmp     .end
+
+.bad:   mov     rax, -1
+
+.end:   leave
+        ret
 
 ; --- RESP_400 (int client_fd) ---
 
@@ -80,6 +79,27 @@ resp_400:
 
         mov     rdx, rax                                ; count
         mov     rsi, resp_header_400                    ; buf
+        mov     rdi, [rsp]
+        mov     rax, 1
+        syscall
+
+        pop     rdi
+        mov     rax, 3
+        syscall
+
+        ret
+
+
+; --- RESP_404 (int client_fd) ---
+
+resp_404:
+        push    rdi
+
+        mov     rdi, resp_header_404
+        call    strlen
+
+        mov     rdx, rax                                ; count
+        mov     rsi, resp_header_404                    ; buf
         mov     rdi, [rsp]
         mov     rax, 1
         syscall
@@ -106,7 +126,5 @@ resp_200:
         syscall
 
         pop     rdi
-        mov     rax, 3
-        syscall
 
         ret
